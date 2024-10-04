cfd7dd40a651d739ebb40f633b9f2aa4be054e6f
Aswanth K <aswanth1611@gmail.com>
2024-10-04 13:16:16 +0530

    perfect

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import "./Banner.css";
 
-function Banner() {
-  return (
-    <div style={{backgroundImage:`url("https://images.indianexpress.com/2024/09/Kishkindha-Kandam-movie-review-12092024.jpg?w=640")`}} className='banner'>
-        <div className='content'>
-           <h1 className='title'>Title</h1>
-           <h1 className='description'>Description</h1>
+function Banner({ movieData }) {
+  const [randomMovie, setRandomMovie] = useState(null);
 
-        </div>
-        <div className='fade_bottom'></div>
+  useEffect(() => {
+    if (movieData && movieData.length > 0) {
+      let validMovie = null;
+
+      // Loop to ensure we get a movie with a valid backdrop_path
+      while (!validMovie) {
+        const randomIndex = Math.floor(Math.random() * movieData.length);
+        const selectedMovie = movieData[randomIndex];
+
+        // Check if the selected movie has a valid backdrop_path
+        if (selectedMovie.backdrop_path) {
+          validMovie = selectedMovie;
+        }
+
+        // Break the loop if all movies lack a backdrop, prevent infinite loop
+        if (movieData.every(movie => !movie.backdrop_path)) {
+          validMovie = { title: "Default Title", description: "Default Description", backdrop_path: "default_image_path.jpg" };
+          break;
+        }
+      }
+      setRandomMovie(validMovie);
+    }
+  }, [movieData]);
+
+  if (!randomMovie) {
+    return null; // Don't render anything if no random movie is selected yet
+  }
+
+  return (
+    <div 
+      style={{ backgroundImage: `url(${randomMovie.imageBase + randomMovie.backdrop_path})` }} 
+      className='banner'
+    >
+      <div className='content'>
+        <h1 className='title'>{randomMovie.title || "No Title Available"}</h1>
+        <h1 className='description'>{randomMovie.overview || "No Description Available"}</h1>
+      </div>
+      <div className='fade_bottom'></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
